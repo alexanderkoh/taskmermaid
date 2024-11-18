fa7ea40155be599c7cef5a066dcead2545bf8182
alexanderkoh <alex@telluscoop.com>
2024-11-18 17:25:11 -0500

    next-themes rm

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,23 +1,11 @@
 'use client'
 
-import { ThemeProvider as NextThemeProvider } from 'next-themes'
 import { type ReactNode } from 'react'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <NextThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem={false}
-      storageKey="taskmermaid-theme"
-      value={{
-        light: 'light',
-        dark: 'dark',
-        blue: 'blue',
-        pink: 'pink'
-      }}
-    >
+    <div>
       {children}
-    </NextThemeProvider>
+    </div>
   )
 } 
